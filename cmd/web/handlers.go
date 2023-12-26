1eca7c3c9f4b29676aa8b5c34cf4d46e207c5715
haidousm <haidous.m@gmail.com>
2023-12-26 12:58:21 -0800

    add form validation

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -5,6 +5,8 @@ import (
 	"fmt"
 	"net/http"
 	"strconv"
+	"strings"
+	"unicode/utf8"
 
 	"binme.haido.us/internal/models"
 	"github.com/julienschmidt/httprouter"
@@ -52,7 +54,17 @@ func (app *application) binView(w http.ResponseWriter, r *http.Request) {
 
 func (app *application) binCreate(w http.ResponseWriter, r *http.Request) {
 	data := app.newTemplateData(r)
+	data.Form = binCreateForm{
+		Expires: 365,
+	}
 	app.render(w, r, http.StatusOK, "new.tmpl", data)
+}
+
+type binCreateForm struct {
+	Title       string
+	Content     string
+	Expires     int
+	FieldErrors map[string]string
 }
 
 func (app *application) binCreatePost(w http.ResponseWriter, r *http.Request) {
@@ -63,15 +75,41 @@ func (app *application) binCreatePost(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	title := r.PostForm.Get("title")
-	content := r.PostForm.Get("content")
 	expires, err := strconv.Atoi(r.PostForm.Get("expires"))
 	if err != nil {
 		app.clientError(w, http.StatusBadRequest)
 		return
 	}
 
-	id, err := app.bins.Insert(title, content, expires)
+	form := binCreateForm{
+		Title:       r.PostForm.Get("title"),
+		Content:     r.PostForm.Get("content"),
+		Expires:     expires,
+		FieldErrors: map[string]string{},
+	}
+
+	if strings.TrimSpace(form.Title) == "" {
+		form.FieldErrors["title"] = "This field cannot be blank"
+	} else if utf8.RuneCountInString(form.Title) > 100 {
+		form.FieldErrors["title"] = "This field cannot be more than 100 characters long"
+	}
+
+	if strings.TrimSpace(form.Content) == "" {
+		form.FieldErrors["content"] = "This field cannot be blank"
+	}
+
+	if form.Expires != 1 && form.Expires != 7 && form.Expires != 365 {
+		form.FieldErrors["expires"] = "This field must equal 1, 7 or 365"
+	}
+
+	if len(form.FieldErrors) > 0 {
+		data := app.newTemplateData(r)
+		data.Form = form
+		app.render(w, r, http.StatusUnprocessableEntity, "new.tmpl", data)
+		return
+	}
+
+	id, err := app.bins.Insert(form.Title, form.Content, form.Expires)
 	if err != nil {
 		app.serverError(w, r, err)
 		return
--- cmd/web/templates.go
+++ cmd/web/templates.go
@@ -12,6 +12,7 @@ type templateData struct {
 	CurrentYear int
 	Bin         models.Bin
 	Bins        []models.Bin
+	Form        any
 }
 
 func humanDate(t time.Time) string {
